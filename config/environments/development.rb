ecb5a169102ca845584165464674ca9eab79b135
MACARONBOOK <ec2-user@ip-172-31-92-247.ec2.internal>
2022-07-17 07:17:35 +0000

    header(branch) - headerとfooter,topとabout作成,ホスト名追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "0fb97966b2ef47e1a617aca0ec12dd70.vfs.cloud9.us-east-1.amazonaws.com"
 end
