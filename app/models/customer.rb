b9fda8b06705467702779c77c06330ac3f6fd337
EC2 Default User <ec2-user@ip-172-31-31-32.ap-northeast-1.compute.internal>
2022-07-17 03:09:19 +0000

    sign_up

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,20 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+
+  validates :email, presence: true
+  validates :post_code, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
+
 end
+
+
