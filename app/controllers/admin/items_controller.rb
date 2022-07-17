9b8e5370223ba733815286768ee848195ef820de
MizuhoMatsushima <105145800+MizuhoMatsushima@users.noreply.github.com>
2022-07-17 14:16:16 +0000

    [Start]enum-introduction

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,18 @@
 class Admin::ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to item_path
+    end
+  end
+
+
+  private
+  def item_params
+    params.require(:book).permit(:title, :body)
+  end
 end
