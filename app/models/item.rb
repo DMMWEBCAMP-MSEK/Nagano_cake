00c2d55a0935e6ab1450dfe5b6b55fc58a474814
MACARONBOOK <ec2-user@ip-172-31-92-247.ec2.internal>
2022-07-18 10:29:32 +0000

    fix item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,6 @@ class Item < ApplicationRecord
   has_many :order_items, dependent: :destroy
 
   enum sales_status: { sale: 0, stop_selling: 1 }
-  enum prdoction_status: { 販売中: true, 販売停止中: false }
   has_one_attached :image
 
   validates :genre_id, :name, :price, presence: true
