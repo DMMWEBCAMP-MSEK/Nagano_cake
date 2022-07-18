b8004b9ba2a9d0be475e933b9ba17ea2fb4f7dad
MACARONBOOK <ec2-user@ip-172-31-92-247.ec2.internal>
2022-07-18 03:46:07 +0000

    publicの商品一覧と商品詳細ページ作成

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,14 @@
 class Public::ItemsController < ApplicationController
+
+def index
+   @items = Item.all.page(params[:page]).reverse_order
+   @genres = Genre.all
+end
+
+def show
+   @item = Item.find(params[:id])
+   @genres = Genre.all
+   @cart_item = CartItem.new
+end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,19 @@
 class Item < ApplicationRecord
+  belongs_to :genre
+  attachment :image
 
+  validates :genre_id, :name, :price, presence: true
+  validates :price, numericality: { only_integer: true }
 
+  enum prdoction_status: { 販売中: true, 販売停止中: false }
+
+  def add_tax_price
+    (self.price * 1.1)
+    # selfにproductを代入
+  end
+
+  # 検索機能使う場合
+  def search
+  end
 
 end
