3b18579fb5c385566d3a72535112f0a55471a2d6
MACARONBOOK <ec2-user@ip-172-31-92-247.ec2.internal>
2022-07-18 07:36:59 +0000

    fix conflict at item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,13 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_many :cart_items, dependent: :destroy
   has_many :order_items, dependent: :destroy
-  
+
+  enum sales_status: { sale: 0, stop_selling: 1 }
+  enum prdoction_status: { 販売中: true, 販売停止中: false }
   has_one_attached :image
 
-<<<<<<< HEAD
   validates :genre_id, :name, :price, presence: true
   validates :price, numericality: { only_integer: true }
-
-  enum prdoction_status: { 販売中: true, 販売停止中: false }
 
   def add_tax_price
     (self.price * 1.1).floor
@@ -19,9 +18,5 @@ class Item < ApplicationRecord
   # 検索機能使う場合
   def search
   end
-=======
-  enum sales_status: { sale: 0, stop_selling: 1 }
-  has_one_attached :image
->>>>>>> develop
 
 end
