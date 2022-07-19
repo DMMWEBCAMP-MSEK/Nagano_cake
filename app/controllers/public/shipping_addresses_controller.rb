bf5ed6b8e9ad8f915adbb2287a2f011f59ed0355
MizuhoMatsushima <105145800+MizuhoMatsushima@users.noreply.github.com>
2022-07-19 07:54:02 +0000

    [Add]

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,2 +1,19 @@
 class Public::ShippingAddressesController < ApplicationController
+
+  def index
+    @shipping_address = ShippingAaddress.new
+    @shipping_addresses = ShippingAddress.all
+  end
+
+  def edit
+    @shipping_address = Shipping_address.find(params[:id])
+  end
+
+  def update
+    @shipping_address = Shipping_address.find(params[:id])
+    if @shipping_address.update
+      redirect_to shipping_addresses_pass
+    end
+  end
+
 end
