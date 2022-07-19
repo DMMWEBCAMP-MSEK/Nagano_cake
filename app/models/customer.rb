147dd1e25933d49cfab66d40ca06b83285ee7dc6
MACARONBOOK <ec2-user@ip-172-31-92-247.ec2.internal>
2022-07-19 02:57:01 +0000

    会員編集作成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -23,7 +23,7 @@ class Customer < ApplicationRecord
   validates :password_confirmation, presence: true, length: { minimum: 6 }
 
   def active_for_authentication?
-    supper && (self.is_deleted == false)
+    super && (self.is_deleted == false)
   end
 
 end
