17e6bd0863634a05619f1c0e547d008d0d5e3687
EC2 Default User <shinya.sin09@gmail.com>
2022-07-19 03:08:34 +0000

    [fix]Customer_Edit

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -14,15 +14,12 @@ class Admin::CustomersController < ApplicationController
 
   def update
    @customer = Customer.find(params[:id])
-   if @customer.update(customer_params)
-     redirect_to admin_customer_path(@customer)
-   else
-     render :edit
-   end
+   @customer.update(customer_params)
+   redirect_to admin_customer_path(@customer)
   end
 
   private
   def customer_params
-    params.require(:customer).permit(:authenticity_token, :first_name, :last_name, :first_name_kana, :last_name_kana, :post_code, :address, :email, :phone_number,  :is_deleted )
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :post_code, :address, :email, :phone_number, :is_deleted )
   end
 end
