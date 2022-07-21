35e2895a71e85965fdbd30d683fe868fe80d396c
EC2 Default User <ec2-user@ip-172-31-31-32.ap-northeast-1.compute.internal>
2022-07-21 13:56:04 +0000

    satoshin omatsu

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,49 +1,38 @@
 class Public::CartItemsController < ApplicationController
 
-  #before_action :set_line_item, only: [:add_item, :destroy]
-  #before_action :set_customer
-  #before_action :set_cart_item
-
   before_action :authenticate_customer!
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    #@cart_item.save
-    #redirect_to cart_items_path
-
-    @cart_items = current_customer.cart_items.new(cart_item_params)
-
-    if    current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
-          cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
-          cart_item.amount += params[:cart_item][:amount].to_i
-          @cart_item.save
-          redirect_to cart_items_path(@cart_items)
-    elsif @cart_item.save
-          @cart_items = current_customer.cart_items.all
-          render 'orders/new'
+    @cart_item = current_customer.cart_items.build(cart_item_params)
+    @cart_items = current_customer.cart_items.all
+    if @cart_item.valid?
+    @cart_items.each do |cart_item|
+      if cart_item.item_id == @cart_item.item_id
+         new_amount = cart_item.amount + @cart_item.amount
+         cart_item.update_attribute(:amount, new_amount)
+         @cart_item.delete
+      end
+      end
+    @cart_item.save
+    redirect_to cart_items_path
     else
-          render 'orders/new'
+    redirect_to request.referer, notice: '数量を変更してください'
     end
 
   end
 
   def index
+    @cart_items = current_customer.cart_items.all
     @cart_items = CartItem.where(customer_id: current_customer.id)
     @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
     @order = Order.new
-    @cart_items = current_customer.cart_items.all
+
   end
 
   def update
-    #@cart_item = CartItem.find(params[:id])
-    #@cart_item.update(cart_item_params)
-    if my_cart_item.update_item(item_id: params[:item_id], amount: params[:amount])
-    redirect_to cart_items_path
-    else
-    redirect_to cart_items_path
-    end
-
+  @cart_item = CartItem.find(params[:id])
+  @cart_item.update(cart_item_params)
+  redirect_to cart_items_path
   end
 
   def destroy_all
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,25 +4,32 @@ class Public::OrdersController < ApplicationController
     @order = Order.new
     @customer = Customer.find(current_customer.id)
     @addresses = current_customer.shipping_addresses.all
-    #@addresses = @customer.shipping_addresses.all
+  end
+
+  def index
+    @orders = current_customer.orders.page(params[:page])
+
+  end
+
+  def show
+
   end
 
   def create
-    @order = Order.new(order_params)
+    @order = current_customer.orders.new(order_params)
+    @addresses = current_customer.shipping_addresses.all
     @cart_items = current_customer.cart_items.all
       if @order.save
-      cart_items.each do |cart_item|
-      order_item = OrderItem.new
-      order_item.item_id = cart_item_id
-      order_item.order_id = @order.id
-      order_item.order_amount = cart_item.amount
-      order_item.order_price = cart_item.price
-      order_item.save
+      @cart_items.each do |cart_item|
+      @order_item = @order.order_items.new
+      @order_item.item_id = cart_item.item.id
+      @order_item.amount = cart_item.amount
+      @order_item.price = cart_item.item.price
+      @order_item.save
         end
-      redirect_to confirm_orders_path
-      cart_items.destroy_all
+      redirect_to thanks_orders_path
+      @cart_items.destroy_all
       else
-      @order = Order.new(order_params)
       render :new
       end
   end
@@ -35,28 +42,16 @@ class Public::OrdersController < ApplicationController
     @order.post_code = current_customer.post_code
     elsif params[:order][:address_number] == "2"
 
-      #if ShippingAddress.exists?(name: params[:order][:registered])
-      #@order.name = ShippingAddress.find(params[:order][:registered]).name
-      #@order.address = ShippingAddress.find(params[:order][:registered]).address
-      #@order.post_code = ShippingAddress.find(params[:order][:registered]).post_code
-      #else
-      #render :new
-      #end
-
       ship = ShippingAddress.find(params[:order][:customer_id])
       @order.post_code = ship.post_code
       @order.address = ship.address
       @order.name = ship.name
 
     elsif params[:order][:address_number] == "3"
-    #address_new = current_customer.address.new(address_params)
-      #if address_new.save
-      #else
-      #render :new
-      #end
-    @order.post_code = params[:order][:post_code]
-    @order.address = params[:order][:address]
-    @order.name = params[:order][:name]
+
+      @order.post_code = params[:order][:post_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
 
     else
       redirect_to new_order_path
@@ -65,7 +60,6 @@ class Public::OrdersController < ApplicationController
     @cart_items = current_customer.cart_items.all
     @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
     @cost = 800
-
   end
 
   def thanks
@@ -74,7 +68,7 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:payment_method, :post_code, :address, :name, :total_price)
+    params.require(:order).permit(:name, :address, :post_code, :payment_method, :total_payment, :shipping_cost)
   end
 
   def address_params
