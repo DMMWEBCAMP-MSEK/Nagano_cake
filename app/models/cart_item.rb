9a8964253043e488a69a80e2a8af1659d3ce0fb5
EC2 Default User <shinya.sin09@gmail.com>
2022-07-21 08:49:02 +0000

    OrderTest

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -4,7 +4,9 @@ class CartItem < ApplicationRecord
 
   belongs_to :item
   belongs_to :customer
-
+  
+  validates :amount, numericality: {only_integer: true, greater_than_or_equal_to: 1}
+  
   def subtotal
     item.with_tax_price * amount
   end
