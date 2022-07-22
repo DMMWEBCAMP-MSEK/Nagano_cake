612a7afd787fecc9b8d9fec128ce09c775801b25
MizuhoMatsushima <105145800+MizuhoMatsushima@users.noreply.github.com>
2022-07-22 07:19:06 +0000

    Update

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,2 +1,16 @@
 class Admin::OrderItemsController < ApplicationController
+
+  def update
+    @order = Order.find(params[:id])
+    @order_item = OrderItem.find(params[:id])
+    @order_item.update(order_item_params)
+    redirect_to admin_order_path(@order)
+  end
+
+
+ private
+
+ def order_item_params
+    params.require(:order_item).permit(:production_status)
+  end
 end
