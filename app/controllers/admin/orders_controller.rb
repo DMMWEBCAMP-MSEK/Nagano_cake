544d6726b9fe8775524707f2b4800ccf6b6f00fc
MizuhoMatsushima <105145800+MizuhoMatsushima@users.noreply.github.com>
2022-07-24 06:48:36 +0000

    Fix

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,10 +7,6 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     if @order.update(order_params)
-      production_status_change_to_1 = @order.status_was == 0 and @order.status == 1
-      if production_status_change_to_1
-        @order.production_status.update_all(production_status: 1)
-      end
       redirect_to admin_order_path(@order)
     else
       render :show
