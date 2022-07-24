001bd179a31b2badc338b0f5870c47140e7b37f3
EC2 Default User <shinya.sin09@gmail.com>
2022-07-24 09:30:43 +0000

    [update]order_status

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -12,7 +12,10 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     if @order.update(order_params)
-      redirect_to admin_order_path(@order_item.order_id)
+      if order_params[:status] == "payment_confirmation"
+        @order.order_items.update_all(production_status:1)
+      end
+      redirect_to admin_order_path(@order)
     else
       render :show
     end
