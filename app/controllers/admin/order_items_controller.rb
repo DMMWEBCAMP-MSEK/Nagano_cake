2816bf8ce5c329397232f44f00de6e46fa05f47b
EC2 Default User <shinya.sin09@gmail.com>
2022-07-24 11:34:19 +0000

    [fix]cart_index_root

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,6 +1,4 @@
 class Admin::OrderItemsController < ApplicationController
-
-  before_action :authenticate_admin!
   def update
     @order_item = OrderItem.find(params[:id])
     @order = @order_item.order
